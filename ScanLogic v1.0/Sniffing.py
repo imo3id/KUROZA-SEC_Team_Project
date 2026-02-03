03cdb291c346a9505f452492e27e7e6fe27efa52
Abdulelah <abdulelahalghuthaimi@gmail.com>
2026-02-03 15:28:11 -0800

    solving a problem

--- ScanLogic v1.0/Sniffing.py
+++ ScanLogic v1.0/Sniffing.py
@@ -5,7 +5,7 @@ from scapy.layers.inet import IP, TCP, UDP
 import threading
 from collections import Counter
 
-*****kالتحقق من انكك ادمن ******
+#*****kالتحقق من انكك ادمن ******
 
 import os
 import sys
@@ -33,7 +33,7 @@ if not is_admin():
         sys.exit()
 
 
-*************
+#*************
 
 captured_packets = []
 ip_counter=Counter()
@@ -54,50 +54,49 @@ def get_packet_info(pkt):
     return (src_ip, dst_ip, proto, length)
 
 
-
 def start_sniffing():
-    interface = iface_box.get()
-    selected_filter = filter_box.get() # الحصول على اختيار المستخدم
-    
-    if not interface:
-        messagebox.showerror("Erorr", "Please choose interface")
-        return
+    interface = iface_box.get()
+    selected_filter = filter_box.get()
 
-    global stop_sniffing_event, captured_packets, ip_counter
-    stop_sniffing_event = False
-    captured_packets = [] 
-    ip_counter.clear()
-    
-    
-    scapy_filter = ""
-    if selected_filter == "TCP": scapy_filter = "tcp"
-    elif selected_filter == "UDP": scapy_filter = "udp"
-    elif selected_filter == "ICMP": scapy_filter = "icmp"
+    if not interface:
+        messagebox.showerror("Error", "Please choose interface")
+        return
 
-    t = threading.Thread(target=process_packets, args=(interface, scapy_filter))
-    t.daemon = True
-    t.start()
+    global stop_sniffing_event, captured_packets, ip_counter
+    stop_sniffing_event = False
+    captured_packets = []
+    ip_counter.clear()
 
-    start_btn.config(state="disabled")
-    stop_btn.config(state="normal")
+    scapy_filter = ""
+    if selected_filter == "TCP":
+        scapy_filter = "tcp"
+    elif selected_filter == "UDP":
+        scapy_filter = "udp"
+    elif selected_filter == "ICMP":
+        scapy_filter = "icmp"
+
+    t = threading.Thread(target=process_packets, args=(interface, scapy_filter))
+    t.daemon = True
+    t.start()
+
+    start_btn.config(state="disabled")
+    stop_btn.config(state="normal")
 
 
-def process_packets(iface, s_filter): 
-    def packet_callback(pkt):
-        if stop_sniffing_event:
-            return True
-        captured_packets.append(pkt)
-        src, dst, proto, length = get_packet_info(pkt)
-        
-        
-        if src != "-":
-            ip_counter[src] += 1
-            
-        table.insert("", "end", values=(src, dst, proto, length))
-        update_stats(proto)
+def process_packets(iface, s_filter):
+    def packet_callback(pkt):
+        if stop_sniffing_event:
+            return True
+        captured_packets.append(pkt)
+        src, dst, proto, length = get_packet_info(pkt)
 
-    
-    sniff(iface=iface, prn=packet_callback, filter=s_filter, stop_filter=lambda x: stop_sniffing_event)
+        if src != "-":
+            ip_counter[src] += 1
+
+        table.insert("", "end", values=(src, dst, proto, length))
+        update_stats(proto)
+
+    sniff(iface=iface, prn=packet_callback, filter=s_filter, stop_filter=lambda x: stop_sniffing_event)
 def stop_capture():
     global stop_sniffing_event
     stop_sniffing_event = True
@@ -117,18 +116,17 @@ stats = {"Total": 0, "TCP": 0, "UDP": 0}
 
 
 def update_stats(proto):
-    stats["Total"] += 1
-    if proto in stats:
-        stats[proto] += 1
+    stats["Total"] += 1
+    if proto in stats:
+        stats[proto] += 1
 
-    total_lbl.config(text=f"Total: {stats['Total']}")
-    tcp_lbl.config(text=f"TCP: {stats['TCP']}")
-    udp_lbl.config(text=f"UDP: {stats['UDP']}")
-    
-   
-    top_3 = ip_counter.most_common(3)
-    top_text = " | ".join([f"{ip}({count})" for ip, count in top_3])
-    top_ip_lbl.config(text=f"Top IPs: {top_text}")
+    total_lbl.config(text=f"Total: {stats['Total']}")
+    tcp_lbl.config(text=f"TCP: {stats['TCP']}")
+    udp_lbl.config(text=f"UDP: {stats['UDP']}")
+
+    top_3 = ip_counter.most_common(3)
+    top_text = " | ".join([f"{ip}({count})" for ip, count in top_3])
+    top_ip_lbl.config(text=f"Top IPs: {top_text}")
 
 def run_sniffer_ui():
     global root, iface_box, start_btn, stop_btn, total_lbl, tcp_lbl, udp_lbl, table , filter_box, top_ip_lbl
